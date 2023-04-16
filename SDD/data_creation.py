f2d904d03b1cbc8586929f245583b82e67fb6b98
arfranck <arthur.franck@student.uclouvain.be>
2023-04-16 18:18:55 +0200

    new size of images, everything set from parameters

--- SDD/data_creation.py
+++ SDD/data_creation.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,40 +6,55 @@ import cv2
 import numpy as np
 from PIL import Image, ImageDraw
 
-
-scene = "bookstore"
-video = "0"
-filename = scene + "/annotations/video" + video + "/annotations.txt" 
-reference_image = scene + "/annotations/video" + video + "/reference.gif" 
+scene = "datasets/bookstore/video0"
+annotation_filename = scene + "/annotations.txt"
 
 cols = ["track_id", "xmin", "ymin", "xmax", "ymax", "frame", "lost", "occluded", "generated", "label"]
-data = pd.read_csv(filename, sep=" ", names=cols)
+data = pd.read_csv(annotation_filename, sep=" ", names=cols)
 
-scale_factor = 8
+
+img_step = 5
+new_size = (128, 128)
+box_size = 8
+
+output_folder = f"{scene}/{new_size[0]}_{new_size[1]}_{box_size}"
+try:
+    os.mkdir(output_folder)
+except FileExistsError as e:
+    pass
+
+new_annotations = pd.DataFrame(columns=cols)
 
 for ind, row in data.iterrows():
-    if (ind % 10 != 0):
+    if (ind % img_step != 0):
         continue
     frame = f"{row.frame:05d}"
     image_path = f"{scene}/frames/{frame}.jpg"
     #print(image_path)
 
     img = Image.open(image_path)
-    img = img.convert("L").resize((img.size[0]//scale_factor, img.size[1]//scale_factor), Image.Resampling.LANCZOS)
+    old_size = img.size
+    img = img.convert("L").resize(new_size, Image.Resampling.LANCZOS)
     draw = ImageDraw.Draw(img)
 
-    margin = 0
-    left = row["xmin"] - margin
-    top = row["ymin"] - margin
-    right = row["xmax"] + margin
-    bottom = row["ymax"] + margin
+    x_scale = old_size[0] / new_size[0]
+    y_scale = old_size[1] / new_size[1]
 
-    #print(row)
-    #print(left, top, right, bottom)
-    #print((left, top, right, bottom))
-    #print((left//scale_factor, top//scale_factor, right//scale_factor, bottom//scale_factor))
-    draw.rectangle((left//scale_factor, top//scale_factor, right//scale_factor, bottom//scale_factor), fill="black")
-    outname = f"{scene}/images/small_bw/{row.track_id:03d}_{frame}.jpg"
-    #print(outname)
-    #image.show()
+    x_center = ((row["xmax"] + row["xmin"]) // 2) / x_scale
+    y_center = ((row["ymax"] + row["ymin"]) // 2) / y_scale
+    left = round(x_center - (box_size//2))
+    top = round(y_center - (box_size//2))
+    right = round(x_center-1 + (box_size//2))
+    bottom = round(y_center-1 + (box_size//2))
+
+    draw.rectangle((left, top, right, bottom), fill="black")
+    outname = f"{output_folder}/{row.track_id:03d}_{frame}.jpg"
     img.save(outname)
+
+    row.xmax = right
+    row.xmin = left
+    row.ymin = top
+    row.ymax = bottom
+    new_annotations.loc[ind] = row
+
+new_annotations.to_csv(output_folder + "/annotations_" + str(img_step) + ".txt", sep=" ", index=False)
