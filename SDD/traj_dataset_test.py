2604c50a19dc45b2c749c2be107edb74ede32ba9
NicolasDepre <nicolas.depre@student.uclouvain.be>
2023-04-26 17:35:24 +0200

    Train and Test working with masking

--- SDD/traj_dataset_test.py
+++ SDD/traj_dataset_test.py
@@ -1,46 +1,38 @@
 import pandas as pd
 import torch
 from torch import nn, Tensor
-from torch.utils.data import dataset, DataLoader
+from torch.utils.data import dataset
 from torchvision import transforms
 
 from PIL import Image
 
-
 class TrajDataset(dataset.Dataset):
-    to_tensor = transforms.ToTensor()
 
+    to_tensor = transforms.ToTensor()
     # cols = ["track_id", "xmin", "ymin", "xmax", "ymax", "frame", "lost", "occluded", "generated", "label"]
 
-    def __init__(self, data_folder, n_prev=4, n_next=6, img_step=10, n_traj=10, device=None,test=False):
+    def __init__(self, data_folder, n_prev=4, n_next=6, img_step=10, n_traj = 10, device=None):
         self.data_folder = data_folder
         self.n_prev = n_prev
         self.n_next = n_next
         self.img_step = img_step
         self.n_traj = n_traj
-        self.test = test
+
         self.src = Tensor()
         self.intermediate = Tensor()
         self.tgt = Tensor()
-        self.device = device
         self.process_data()
-
-
-
+        self.device = device
 
     def process_data(self):
         raw_data = pd.read_csv(self.data_folder + "/annotations_" + str(self.img_step) + ".txt", sep=" ")
-        raw_data[["xmin", "ymin", "xmax", "ymax"]] = self.normalize_coords(raw_data[["xmin", "ymin", "xmax", "ymax"]])
-        # print(raw_data)
-        # raw_data = raw_data[raw_data.index % self.img_step == 0]
+        #print(raw_data)
+        #raw_data = raw_data[raw_data.index % self.img_step == 0]
         src = []
         intermediate = []
         tgt = []
-
-        if self.test:
-            track_ids = raw_data["track_id"].unique()[-self.n_traj:]
-        else:
-            track_ids = raw_data["track_id"].unique()[:self.n_traj]
+        raw_data[["xmin", "ymin", "xmax", "ymax"]] = self.normalize(raw_data[["xmin", "ymin", "xmax", "ymax"]])
+        track_ids = raw_data["track_id"].unique()[:self.n_traj]
 
         for track_id in track_ids:
             print("opening track " + str(track_id))
@@ -55,24 +47,22 @@ class TrajDataset(dataset.Dataset):
                 x2 = self.get_n_images_after_i(traj, self.n_next, self.n_prev + i, memo)
                 intermediate.append(self.normalize_img(x2))
                 # bounding boxes inside predicted images
-                y = traj.iloc[i + self.n_prev: i + self.n_prev + self.n_next][
-                    ["xmin", "ymin", "xmax", "ymax"]]  # recuperer le grand truth à prédire
-                tgt.append(Tensor(y.values))  # add to ground truth dataset
+                y = traj.iloc[i + self.n_prev: i + self.n_prev + self.n_next][["xmin", "ymin", "xmax", "ymax"]]  # recuperer le grand truth à prédire
+                tgt.append(Tensor(self.normalize_coords(y.values))) # add to ground truth dataset
 
         self.src = torch.stack(src, dim=0)
         self.intermediate = torch.stack(intermediate, dim=0)
         self.tgt = torch.stack(tgt, dim=0)
 
     def normalize_img(self, img):
-        return img
+        return img / img.norm(p=2)
 
-    def normalize_coords(self, df):
-        return df
+    def normalize(self, df):
         return (df - df.min()) / (df.max() - df.min())
 
     def get_n_images_after_i(self, traj, n, i, memo):
         X = []
-        for ind, pos in traj.iloc[i: i + n, :].iterrows():
+        for ind, pos in traj.iloc[i: i+n,:].iterrows():
             track_id = pos["track_id"]
             frame = pos["frame"]
             path = f"{self.data_folder}/{track_id:03d}_{frame:05d}.jpg"
@@ -82,7 +72,6 @@ class TrajDataset(dataset.Dataset):
                 img = Image.open(f"{self.data_folder}/{track_id:03d}_{frame:05d}.jpg")
                 memo[path] = img
             img_tensor = self.to_tensor(img)
-            print(img_tensor)
             X.append(img_tensor)
         return torch.cat(X)
 
@@ -99,6 +88,6 @@ class TrajDataset(dataset.Dataset):
         return self.src[0].size()[1:]
 
     def get_dataset_infos(self):
-        return {"image_size": self.get_image_size(),
+        return {"image_size":self.get_image_size(),
                 "n_prev": self.n_prev,
                 "n_next": self.n_next}
