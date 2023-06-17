c600fca47f99b224226736c93f0c71e64b131dd1
Arthur Franck <arthur.franck@student.uclouvain.be>
2023-06-18 00:07:11 +0200

    updated fom other branch

--- SDD/plotting.py
+++ SDD/plotting.py
@@ -1,25 +1,35 @@
 import matplotlib.pyplot as plt
-
+import os
 
 def make_plot(title, training, validation=None, ylim=None):
-    os.system("mkdir plots")
+    try:
+        os.mkdir("plots")
+    except FileExistsError:
+        pass
     feature = title.split('-')[0]
     fig, ax = plt.subplots()
 
     avg_training = [sum(sublst) / len(sublst) for sublst in training]
 
     flat_training = flat_list(training)
-    print(len(flat_training))
-
+    print("len flat: ", len(flat_training))
     if ylim is not None:
         plt.ylim(ylim)
 
     update_steps = range(0, len(flat_training))
+    
+    n_steps = len(flat_training)
+    n_epochs = len(training)
 
     ax.plot(update_steps, flat_training, label='training ' + feature)
-    ax.plot(range(0, len(flat_training), len(training[0])), avg_training, label='training avg per epoch ' + feature)
+    ax.plot(range(0, n_steps, n_steps//n_epochs), avg_training, label='training avg per epoch ' + feature)
+
     if validation is not None:
-        ax.plot(update_steps, validation, label='validation ' + feature)
+        #flat_validation = flat_list(validation)
+        #avg_validation = [sum(sublst) / len(sublst) for sublst in validation]
+        ax.plot(range(0, n_steps, n_steps // n_epochs), validation, label='validation avg per epoch ' + feature)
+        #ax.plot(update_steps, flat_validation, label='validation ' + feature)
+        #ax.plot(range(0, len(flat_validation), len(validation[0])), avg_validation, label='validation avg per epoch ' + feature)
 
     ax.legend()
 
