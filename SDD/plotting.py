acfa4cfc5f4513af765d96ecaead498b51500a8c
arfranck <arthur.franck@student.uclouvain.be>
2023-06-19 19:01:19 +0200

    removed old code

--- SDD/plotting.py
+++ SDD/plotting.py
@@ -1,90 +1,7 @@
 import matplotlib.pyplot as plt
 import os
 import wandb
-
-
-def make_plot(title, training, validation=None, ylim=None):
-    try:
-        os.mkdir("plots")
-    except FileExistsError:
-        pass
-    feature = title.split('-')[0]
-    fig, ax = plt.subplots()
-
-    avg_training = [sum(sublst) / len(sublst) for sublst in training]
-
-    flat_training = flat_list(training)
-    print("len flat: ", len(flat_training))
-    if ylim is not None:
-        plt.ylim(ylim)
-
-    update_steps = range(0, len(flat_training))
-    
-    n_steps = len(flat_training)
-    n_epochs = len(training)
-
-    ax.plot(update_steps, flat_training, label='training ' + feature)
-    ax.plot(range(0, n_steps, n_steps//n_epochs), avg_training, label='training avg per epoch ' + feature)
-
-    if validation is not None:
-        #flat_validation = flat_list(validation)
-        #avg_validation = [sum(sublst) / len(sublst) for sublst in validation]
-        ax.plot(range(0, n_steps, n_steps // n_epochs), validation, label='validation avg per epoch ' + feature)
-        #ax.plot(update_steps, flat_validation, label='validation ' + feature)
-        #ax.plot(range(0, len(flat_validation), len(validation[0])), avg_validation, label='validation avg per epoch ' + feature)
-
-    ax.legend()
-
-    plt.xlabel('update step')
-    plt.ylabel(title)
-
-    filename = get_filename_from_params(title)
-    save_plot(filename, fig)
-    plt.show()
-    plt.close(fig)
-    return
-
-
-def plot_accuracies(training_accuracies, validation_accuracies=None):
-    make_plot("accuracy", training_accuracies, validation_accuracies)
-
-
-def plot_losses(training_losses, validation_losses=None):
-    make_plot("Loss", training_losses, validation_losses)
-
-
-def get_filename_from_params(title):
-    fn = "plots/" + title
-    fn = fn.replace('.', ',')
-    return fn + ".pdf"
-
-
-def save_plot(filename, fig):
-    saved = False
-    while not saved:
-        try:
-            plt.savefig(filename)
-            saved = True
-        except PermissionError:
-            print("Problem with filename")
-            filename = filename.replace(".pdf", "_bis.pdf")
-    return
-
-
-def flat_list(lst):
-    try:
-        return [elem for sublst in lst for elem in sublst]
-    except Exception as e:
-        print(e)
-        return lst
-
 import pandas as pd
-
-#endpoint = "/depren/thesis_official_runs/runs/"
-#run_ids = ["dmn0il2i"]
-#runs = [api.run(endpoint + id) for id in run_ids]
-#histories = [run.history().sort_values(['_step']) for run in runs]
-
 import argparse
 
 def parse_args():
@@ -104,6 +21,7 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+
 def get_title(args):
     if args.title is not None:
         return args.title
@@ -121,6 +39,16 @@ def get_title(args):
         raise Exception("Please provide a title")
     return title
 
+def save_plot(filename, plt):
+    saved = False
+    while not saved:
+        try:
+            plt.savefig(filename)
+            saved = True
+        except PermissionError:
+            print("Problem with filename")
+            filename = filename.replace(".pdf", "_bis.pdf")
+    return
 
 if __name__ == "__main__":
     args = parse_args()
@@ -149,5 +77,5 @@ if __name__ == "__main__":
     plt.title(title)
     plt.legend()
     plt.yscale(yscale)
-    plt.savefig(f"plots/{tags}_{to_plot}.pdf")
+    save_plot(f"plots/{tags}_{to_plot}.pdf", plt)
     plt.show()
